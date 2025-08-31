a4e12f0b7b1665c02bbf74a7d0a669b470b85102
Utkarsh <utkarshsharma3131@gmail.com>
2025-09-01 00:23:40 +0530

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,9 @@ const Footer = () => {
               className="h-[56px] w-[56px]"
             />
             <div className="flex flex-col leading-tight">
-              <h1 className="font-serif font-bold text-lg md:text-[26px] text-white">EAGLE SERVICE CENTRE</h1>
+              <h1 className="font-serif font-bold text-lg md:text-[26px] text-white">
+                EAGLE SERVICE CENTRE
+              </h1>
               <span className="font-serif text-sm text-white md:text-base text-white -mt-1.5">
                 Built with Quality, Grown through Trust..!
               </span>
@@ -48,85 +50,89 @@ const Footer = () => {
             >
               +91-93101-45485
             </Link>
+            <Link
+              href={"mailto: eagleservicecentre77@yahoo.com"}
+              className="z-100 font-light text-[16px] sm:text-[18px] text-[#c7c7c7] hover:underline hover:text-[#696764]"
+            >
+              eagleservicecentre77@yahoo.com
+            </Link>
+
+            {/* Address */}
+            <a
+              href="https://www.google.com/maps/dir//EAGLE+SERVICE+CENTRE,+53B%2F7,+Rama+Rd,+near+Moti+Nagar,+opposite+555+soap+factory,+Block+B,+Industrial+Area,+New+Delhi,+Delhi,+110015/@28.686479,77.034118,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0x390d03a60b193caf:0xc6ff0860d41cae79!2m2!1d77.156688!2d28.6618039!3e0?hl=en&entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="z-100 font-light text-[16px] sm:text-[18px] text-[#c7c7c7] text-left hover:underline hover:text-[#696764]"
+            >
+              53B/7, Rama Rd, near Moti Nagar, opposite 555 soap factory, Block
+              B, Industrial Area, New Delhi, Delhi, 110015
+            </a>
+            <div className="flex gap-4 md:gap-6 lg:mt-5">
+              <div className="flex w-10 h-10 sm:w-12 sm:h-12 p-[10px] sm:p-[14px] bg-[#2c2c2c] rounded-full hover:bg-[#888888]">
+                <Link
+                  href={"https://www.facebook.com/share/16zYuBJ4gs/"}
+                  className="font-light text-[18px] text-[#c7c7c7]"
+                >
+                  <img
+                    src="/facebook-logo.svg"
+                    alt="facebook-logo"
+                    className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
+                  />
+                </Link>
+              </div>
+              <div className="flex w-10 h-10 sm:w-12 sm:h-12 p-[10px] sm:p-[14px] bg-[#2c2c2c] rounded-full hover:bg-[#888888]">
+                <Link
+                  href={"https://www.youtube.com/@eagleservicecentre77"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-light text-[18px] text-[#c7c7c7]"
+                >
+                  <img
+                    src="/youtube-logo.svg"
+                    alt="youtube-logo"
+                    className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
+                  />
+                </Link>
+              </div>
+              <div className="flex w-10 h-10 sm:w-12 sm:h-12 p-[10px] sm:p-[14px] bg-[#2c2c2c] rounded-full hover:bg-[#888888]">
+                <Link
+                  href={"https://wa.me/message/2XA3M6OFIC7TK1"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-light text-[18px] text-[#c7c7c7]"
+                >
+                  <img
+                    src="/whatsapp.svg"
+                    alt="whatsapp-logo"
+                    className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
+                  />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
 
         {/* Right side: Email, Address, Socials */}
-        <div className="flex flex-col gap-6 items-start md:items-end w-full md:w-auto">
-          <Link
-            href={"mailto: eagleservicecentre77@yahoo.com"}
-            className="z-100 font-light text-[16px] sm:text-[18px] text-[#c7c7c7] hover:underline hover:text-[#696764]"
-          >
-            eagleservicecentre77@yahoo.com
-          </Link>
-
-          {/* Address */}
-          <a
-            href="https://www.google.com/maps/dir//EAGLE+SERVICE+CENTRE,+53B%2F7,+Rama+Rd,+near+Moti+Nagar,+opposite+555+soap+factory,+Block+B,+Industrial+Area,+New+Delhi,+Delhi,+110015/@28.686479,77.034118,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0x390d03a60b193caf:0xc6ff0860d41cae79!2m2!1d77.156688!2d28.6618039!3e0?hl=en&entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="z-100 font-light text-[16px] sm:text-[18px] text-[#c7c7c7] text-left md:text-right hover:underline hover:text-[#696764]"
-          >
-            53B/7, Rama Rd, near Moti Nagar, opposite 555 soap factory, Block B,
-            Industrial Area, New Delhi, Delhi, 110015
-          </a>
-
-          {/* Social Icons */}
-          <div className="flex gap-4 md:gap-6">
-            <div className="flex w-10 h-10 sm:w-12 sm:h-12 p-[10px] sm:p-[14px] bg-[#2c2c2c] rounded-full hover:bg-[#888888]">
-              <Link
-                href={"https://www.facebook.com/share/16zYuBJ4gs/"}
-                className="font-light text-[18px] text-[#c7c7c7]"
-              >
-                <img
-                  src="/facebook-logo.svg"
-                  alt="facebook-logo"
-                  className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
-                />
-              </Link>
-            </div>
-            <div className="flex w-10 h-10 sm:w-12 sm:h-12 p-[10px] sm:p-[14px] bg-[#2c2c2c] rounded-full hover:bg-[#888888]">
-              <Link
-                href={"https://www.youtube.com/@eagleservicecentre77"}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-light text-[18px] text-[#c7c7c7]"
-              >
-                <img
-                  src="/youtube-logo.svg"
-                  alt="youtube-logo"
-                  className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
-                />
-              </Link>
-            </div>
-            <div className="flex w-10 h-10 sm:w-12 sm:h-12 p-[10px] sm:p-[14px] bg-[#2c2c2c] rounded-full hover:bg-[#888888]">
-              <Link
-                href={"https://wa.me/message/2XA3M6OFIC7TK1"}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-light text-[18px] text-[#c7c7c7]"
-              >
-                <img
-                  src="/whatsapp.svg"
-                  alt="whatsapp-logo"
-                  className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
-                />
-              </Link>
-            </div>
-
-            {/* <div className="flex w-10 h-10 sm:w-12 sm:h-12 p-[10px] sm:p-[14px] bg-[#2c2c2c] rounded-full hover:bg-[#888888]">
-              <Link
-                href={"mailto:eagleservicecentre77@yahoo.com"}
-                className="font-light text-[18px] text-[#c7c7c7]"
-              >
-                <img
-                  src="/mail.svg"
-                  alt="mail-logo"
-                  className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
-                />
-              </Link>
-            </div> */}
-          </div>
+        <div className="flex flex-col gap-2 items-start md:items-end w-full md:w-auto text-left lg:mt-12">
+          {[
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+          ].map((day) => (
+            <p
+              key={day}
+              className="flex justify-between w-full md:w-52 text-lg"
+            >
+              <span className="font-light text-[#c7c7c7]">{day}</span>
+              <span className="ml-4 font-light text-[#c7c7c7]">
+                {day !== "Sunday" ? "10am - 7pm" : "Closed"}
+              </span>
+            </p>
+          ))}
         </div>
       </div>
 
